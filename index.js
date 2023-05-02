44e0b0549a091cfe194d86286a99d2435f767461
Vel MAr <velmar.annabell@gmail.com>
2023-05-02 15:35:19 +0300

    use slugify and nodemon

--- index.js
+++ index.js
@@ -1,25 +1,9 @@
 const http = require('http');
 const url = require('url');
 const fs = require('fs');
+const slugify = require('slugify');
 
-
-const replaceTemplate = (temp, product) => {
-    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%DESCRIPTION%}/g, product.description);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%ID%}/g, product.id);
-
-    if (!product.organic) {
-        output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-    };
-
-    return output;
-}
+const replaceTemplate = require('./modules/replaceTemplate');
 
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
@@ -30,13 +14,16 @@ const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 
 const dataObj = JSON.parse(data);
 
+const slugs = dataObj.map(x => slugify(x.productName, { lower: true }))
+
+console.log(slugs);
 const server = http.createServer((req, res) => {
     // console.log(req.url);
     // console.log(url.parse(req.url,true ));
-   const {query, pathname} = url.parse(req.url,true);
+    const { query, pathname } = url.parse(req.url, true);
 
 
-    const pathName = req.url;
+    // const pathName = req.url;
     //Overview page
     if (pathname === '/' || pathname === '/overview') {
         res.writeHead(200, { 'Content-type': 'text/html' });
@@ -50,7 +37,7 @@ const server = http.createServer((req, res) => {
     } else if (pathname === '/product') {
         res.writeHead(200, { 'Content-type': 'text/html' });
         const product = dataObj[query.id];
-        const output = replaceTemplate(tempProduct,product);
+        const output = replaceTemplate(tempProduct, product);
 
         res.end(output);
 
@@ -71,4 +58,4 @@ const server = http.createServer((req, res) => {
 
 server.listen(8000, '127.0.0.1', () => {
     console.log('Listening to request on port 8000');
-})
+});
